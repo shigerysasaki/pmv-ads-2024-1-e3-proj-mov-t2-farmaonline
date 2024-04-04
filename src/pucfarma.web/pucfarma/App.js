492e786b13dbc6ecd78105138a8ca4904e6b6cbd
renanstankev <renan.stankevicius@gmail.com>
2024-04-04 13:12:24 -0300

    cria base do projeto react-native

---
+++ src/pucfarma.web/pucfarma/App.js
@@ -0,0 +1,26 @@
+import 'react-native-gesture-handler';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+import Cadastro from './pages/autenticacao/cadastro/Cadastro';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+return (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Cadastro" component={Cadastro} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+}
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
+},
+});
