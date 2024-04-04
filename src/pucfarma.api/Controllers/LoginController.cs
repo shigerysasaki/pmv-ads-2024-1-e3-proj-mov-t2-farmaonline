c568daf59e52afd778d979d536defedf0a88f4a3
renanstankev <renan.stankevicius@gmail.com>
2024-04-04 18:24:34 -0300

    feat: adiciona controllers de cadastro e login na api

---
+++ src/pucfarma.api/Controllers/LoginController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pucfarma.api.Data;
+using pucfarma.api.Models;
+
+namespace pucfarma.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutenticacaoController : ControllerBase
+    {
+        private readonly PucFarmaDbContext _context;
+
+        public AutenticacaoController(PucFarmaDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] UsuarioModel usuarioLogin)
+        {
+            if (usuarioLogin == null || string.IsNullOrWhiteSpace(usuarioLogin.email) || string.IsNullOrWhiteSpace(usuarioLogin.senha))
+            {
+                return BadRequest(new { message = "E-mail e senha são obrigatórios para o login." });
+            }
+
+            var dados = await _context.Usuarios
+                .Where(u => u.email == usuarioLogin.email)
+                .FirstOrDefaultAsync();
+
+            if (dados == null || dados.senha != usuarioLogin.senha)
+            {
+                return Unauthorized(new { message = "Credenciais inválidas." });
+            }
+
+            var claims = new List<Claim>
+    {
+        new Claim(ClaimTypes.NameIdentifier, dados.usuarioId.ToString()),
+        new Claim(ClaimTypes.Name, dados.nomeCompleto),
+        new Claim(ClaimTypes.Email, dados.email),
+    };
+
+            var userIdentity = new ClaimsIdentity(claims, "login");
+
+            var principal = new ClaimsPrincipal(userIdentity);
+
+            await HttpContext.SignInAsync(principal);
+
+            return Ok(new { message = "Login bem-sucedido." });
+        }
+
+
+        [HttpPost("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+
+            return Ok(new { message = "Logout bem-sucedido." });
+        }
+    }
+}
--- src/pucfarma.api/Program.cs
+++ src/pucfarma.api/Program.cs
@@ -3,14 +3,30 @@ using pucfarma.api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<PucFarmaDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthentication("CookieAuth")
+    .AddCookie("CookieAuth", config =>
+    {
+        config.Cookie.Name = "MyCookie";
+        config.LoginPath = "/api/Login"; // Defina o caminho para o método Login do seu controlador
+    });
+
+builder.Services.AddDistributedMemoryCache(); // Adicionar esta linha para registrar o serviço IDistributedCache
+
+builder.Services.AddSession();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
@@ -23,6 +39,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
