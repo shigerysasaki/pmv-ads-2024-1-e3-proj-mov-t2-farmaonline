4e181bb7ebb708a6aff732dc8441d79950c4a38f
shigerysasaki <shigerysasaki12@hotmail.com>
2024-04-07 17:25:22 -0300

    add tela de login, add alertas de erro, add rotas de tela

--- src/pucfarma.web/pucfarma/App.js
+++ src/pucfarma.web/pucfarma/App.js
@@ -3,24 +3,29 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack";
 import Cadastro from './pages/autenticacao/cadastro/Cadastro';
+import Login from './pages/autenticacao/cadastro/Login';
+
 
 const Stack = createStackNavigator();
 
 export default function App() {
-return (
-  <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name=" " component={Cadastro} />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Cadastro" component={Cadastro} />
+        
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  backgroundColor: '#fff',
-  alignItems: 'center',
-  justifyContent: 'center',
-},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
