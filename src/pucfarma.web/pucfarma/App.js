8a29b980fb978358cdc35f808062ce86f6ca8714
MatheusPuc <matheus.silva.1456191@sga.pucminas.br>
2024-04-19 20:22:51 -0400

    Adicionado_template

--- src/pucfarma.web/pucfarma/App.js
+++ src/pucfarma.web/pucfarma/App.js
@@ -1,9 +1,10 @@
 import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack";
 import Cadastro from './pages/autenticacao/cadastro/Cadastro';
 import Login from './pages/autenticacao/cadastro/Login';
+import Mais from './pages/telas/mais';
 
 
 const Stack = createStackNavigator();
@@ -13,8 +14,13 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Cadastro" component={Cadastro} />
-        
+        <Stack.Screen name="Cadastro" component={Cadastro}/>
+        <Stack.Screen name="Mais" component={Mais} options={{
+          headerShown: false,
+          headerTransparent: true,
+          title: '',
+        }}/>
+
         
       </Stack.Navigator>
     </NavigationContainer>
---
+++ src/pucfarma.web/pucfarma/pages/template/footer.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, TouchableOpacity, Text, Image } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+const Footer = () => {
+  const navigation = useNavigation();
+
+  const [activeTab, setActiveTab] = useState('Home');
+
+  const handleTabPress = (tab) => {
+    setActiveTab(tab);
+    navigation.navigate(tab);
+  };
+
+  return (
+    <View style={styles.footer}>
+      <TouchableOpacity
+        style={[styles.tab, activeTab === 'Home' && styles.active]}
+        onPress={() => handleTabPress('Home')}>
+        {activeTab === 'Home' ? (
+          <Image source={require('../../assets/homeactive.png')} style={styles.icon} />
+        ) : (
+          <Image source={require('../../assets/home.png')} style={styles.icon} />
+        )}
+        <Text style={[styles.tabLabel, activeTab === 'Home' && styles.activeTabLabel]}>Home</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.tab, activeTab === 'Ofertas' && styles.active]}
+        onPress={() => handleTabPress('Ofertas')}>
+        {activeTab === 'Ofertas' ? (
+          <Image source={require('../../assets/oferactive.png')} style={styles.icon} />
+        ) : (
+          <Image source={require('../../assets/ofer.png')} style={styles.icon} />
+        )}
+        <Text style={[styles.tabLabel, activeTab === 'Ofertas' && styles.activeTabLabel]}>Ofertas</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.tab, activeTab === 'Carrinho' && styles.active]}
+        onPress={() => handleTabPress('Carrinho')}>
+        {activeTab === 'Carrinho' ? (
+          <Image source={require('../../assets/cartactive.png')} style={styles.icon} />
+        ) : (
+          <Image source={require('../../assets/cart.png')} style={styles.icon} />
+        )}
+        <Text style={[styles.tabLabel, activeTab === 'Carrinho' && styles.activeTabLabel]}>Carrinho</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.tab, activeTab === 'Mais' && styles.active]}
+        onPress={() => handleTabPress('Mais')}>
+        {activeTab === 'Mais' ? (
+          <Image source={require('../../assets/moreactive.png')} style={styles.icon} />
+        ) : (
+          <Image source={require('../../assets/more.png')} style={styles.icon} />
+        )}
+        <Text style={[styles.tabLabel, activeTab === 'Mais' && styles.activeTabLabel]}>Mais</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    height: '10%',
+    backgroundColor: '#FFFFFF',
+    borderWidth: 1,
+    borderRadius: 3,
+    borderColor: '#EEEEEE',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  tab: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  active: {
+    backgroundColor: '#FFFFFF',
+  },
+  tabLabel: {
+    fontSize: 13,
+    fontWeight: 'bold',
+    color: '#898989',
+  },
+  activeTabLabel: {
+    color: '#74b0ff'
+  },
+  icon: {
+    width: 30,
+    height: 30,
+  },
+});
+
+export default Footer;
