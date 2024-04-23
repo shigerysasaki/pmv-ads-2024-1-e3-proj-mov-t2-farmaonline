08c60273db439ba5497854cef7c4a8fc64d2e0d5
MatheusPuc <matheus.silva.1456191@sga.pucminas.br>
2024-04-22 20:03:49 -0400

    Add_footeradm

--- src/pucfarma.web/pucfarma/pages/autenticacao/cadastro/Login.js
+++ src/pucfarma.web/pucfarma/pages/autenticacao/cadastro/Login.js
@@ -137,7 +137,7 @@ export default function Login() {
 
         <Text style={styles.textoConta}>Ainda não possui conta?</Text>
 
-        <TouchableOpacity style={styles.btnSubmit} onPress={() => navigation.navigate('EditarProduto')}>
+        <TouchableOpacity style={styles.btnSubmit} onPress={() => navigation.navigate('Mais')}>
           <Text style={styles.submitText}>Criar Conta</Text>
         </TouchableOpacity>
 
