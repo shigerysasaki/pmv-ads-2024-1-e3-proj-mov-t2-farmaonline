8797e7fb35f68144571d7abcf67ce7c5c80abc5f
MatheusPuc <matheus.silva.1456191@sga.pucminas.br>
2024-05-03 19:27:02 -0400

    front_carrinho

--- src/pucfarma.web/pucfarma/App.js
+++ src/pucfarma.web/pucfarma/App.js
@@ -2,16 +2,16 @@ import 'react-native-gesture-handler';
 import { StyleSheet, Text, View, } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack";
+
 import Cadastro from './pages/autenticacao/cadastro/Cadastro';
 import Login from './pages/autenticacao/cadastro/Login';
 import Home from './pages/autenticacao/cadastro/Home';
 import EditarProduto from './pages/telas/EditarProduto';
 import Mais from './pages/telas/mais';
-
 import Produtos from './pages/produtos/Produtos';
-
 import Andamento from './pages/telas/andamento';
 import Historico from './pages/telas/Historico'
+import Carrinho from './pages/telas/Carrinho';
 
 const Stack = createStackNavigator();
 
@@ -19,19 +19,23 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen name="Produtos" component={Produtos} />
-      <Stack.Screen name="EditarProduto" component={EditarProduto} />
+        <Stack.Screen name="Carrinho" component={Carrinho} options={{
+          headerShown: false,
+          headerTransparent: true,
+          title: '',
+        }} />
+        <Stack.Screen name="Produtos" component={Produtos} />
+        <Stack.Screen name="EditarProduto" component={EditarProduto} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Cadastro" component={Cadastro} />
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="EditarProduto" component={EditarProduto} />
         <Stack.Screen name="Historico" component={Historico} />
         <Stack.Screen name="Mais" component={Mais} options={{
           headerShown: false,
           headerTransparent: true,
           title: '',
         }} />
-        <Stack.Screen name="andamento" component={Andamento}/>
+        <Stack.Screen name="andamento" component={Andamento} />
       </Stack.Navigator>
     </NavigationContainer>
   );
