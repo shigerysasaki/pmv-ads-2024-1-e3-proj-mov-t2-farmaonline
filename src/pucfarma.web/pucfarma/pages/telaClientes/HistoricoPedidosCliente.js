53e3bfefdc16a34904f93719ac0c99e0ee9625a8
Natali Costa <natalixcosta@icloud.com>
2024-06-13 11:44:20 -0300

    Mudanças de front

--- src/pucfarma.web/pucfarma/pages/telaClientes/HistoricoPedidosCliente.js
+++ src/pucfarma.web/pucfarma/pages/telaClientes/HistoricoPedidosCliente.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Image, Alert } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
 import Footer from '../template/footer';
 
 const HistoricoPedidos = () => {
-    const [pedidos, setPedidos] = useState([]);
 
+    const [pedidos, setPedidos] = useState([]);
+   
     useEffect(() => {
         handleGetPedidosUsuario();
     }, []);
@@ -23,12 +24,15 @@ const HistoricoPedidos = () => {
                 throw new Error('Erro na requisição: ' + response.status + ' - ' + errorText);
             }
 
-            const data = await response.json();
+            const responseText = await response.text();
+
+            if (!responseText) {
+                throw new Error('Resposta vazia do servidor.');
+            }
+
+            const data = JSON.parse(responseText);
             console.log(data);
             setPedidos(data);
-            data.forEach(pedido => {
-                handleGetPedidoProduto(pedido.pedidoId);
-            });
         } catch (error) {
             console.error('Erro:', error);
             Alert.alert('Erro', 'Não foi possível obter os dados do usuário. Por favor, tente novamente mais tarde.');
@@ -51,7 +55,7 @@ const HistoricoPedidos = () => {
 
             const data = await response.json();
             console.log(data);
-            setPedidos(prevPedidos => prevPedidos.map(pedido => 
+            setPedidos(prevPedidos => prevPedidos.map(pedido =>
                 pedido.pedidoId === pedidoId ? { ...pedido, produtos: data } : pedido
             ));
         } catch (error) {
@@ -63,7 +67,7 @@ const HistoricoPedidos = () => {
     const formatarData = (dataString) => {
         const data = new Date(dataString);
         const dia = String(data.getDate()).padStart(2, '0');
-        const mes = String(data.getMonth() + 1).padStart(2, '0'); 
+        const mes = String(data.getMonth() + 1).padStart(2, '0');
         const ano = data.getFullYear();
         return `${dia}/${mes}/${ano}`;
     };
@@ -87,42 +91,53 @@ const HistoricoPedidos = () => {
 
     return (
         <View style={styles.container}>
-            <View style={styles.pedidoContainer}>
-                {pedidos.map(pedido => (
-                    <View key={pedido.pedidoId} style={styles.pedidoItem}>
-                        <Text style={{ color: '#898989' }}>Id do pedido: {pedido.pedidoId}</Text>
-                        <Text style={{ color: '#898989' }}>Data da compra: {formatarData(pedido.dataPedido)}</Text>
-                        <Text style={{ color: '#898989' }}>Previsão de entrega: {formatarData(pedido.previsaoEntrega)}</Text>
-                        <Text style={{ color: '#898989' }}>Método de pagamento: {formatarMetodoPagamento(pedido.metodoPagamento)}</Text>
-                        {pedido.produtos && pedido.produtos.map(produto => (
-                            <View key={produto.pedidoProdutoId} style={styles.produtoItem}>
-                                <Image source={{ uri: produto.produto.fotoProduto }} style={styles.avatar} />
-                                <View style={styles.infoContainer}>
-                                    <Text style={styles.nome}>{produto.produto.nomeProduto}</Text>
-                                    <Text style={styles.preco}>Preço individual: {produto.produto.preco}</Text>
-                                    <Text style={styles.quantidade}>Quantidade: {produto.quantidade}</Text>
-                                </View>
+            <ScrollView contentContainerStyle={styles.scrollContent}>
+                <View style={styles.pedidoContainer}>
+                    {pedidos.map(produto => (
+                        <View key={produto.produtoId} style={styles.pedidoItem}>
+                            <Image source={{ uri: produto.fotoProduto }} style={styles.avatar} />
+                            <View style={styles.infoContainer}>
+                                <Text style={styles.nome}>{produto.nomeProduto}</Text>
+                                <Text style={styles.preco}>Preço individual: {produto.preco}</Text>
+                                <Text style={styles.total}>Valor total: <Text style={styles.greenText}>{produto.total}</Text></Text>
                             </View>
-                        ))}
-                    </View>
-                ))}
-            </View>
-            <Footer />
+                            <Text style={styles.quantidade}>Quantidade: {produto.quantidade}</Text>
+                        </View>
+                    ))}
+
+                    {pedidos.map(pedido => (
+                        <><View style={styles.additionalInfo}>
+                            <Text style={{ color: '#898989' }}>Id do pedido: {pedido.pedidoId}</Text>
+                            <Text style={{ color: '#898989' }}>Data da compra: {formatarData(pedido.dataPedido)}</Text>
+                            <Text style={{ color: '#898989' }}>Previsão de entrega: {formatarData(pedido.previsaoEntrega)}</Text>
+                            <Text style={{ color: '#898989' }}>Método de pagamento: {formatarMetodoPagamento(pedido.metodoPagamento)}</Text>
+                        </View><View>
+                                <Text style={styles.totalCompra}>
+                                    Valor total da compra: <Text style={styles.greenText}></Text>
+                                </Text>
+
+                                <Text style={{ color: '#898989', textAlign: 'center', fontSize: 16, marginTop: 20, marginBottom: 20 }}>
+                                    Pedido entregue em: <Text style={{ color: '#0061E1' }}>{formatarData(pedido.previsaoEntrega)}</Text>
+                                </Text>
+                            </View></>
+                    ))}
+                </View>
+            </ScrollView>
+            <Footer style={styles.footer} />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        flex: 1,  
         paddingTop: 20,
     },
+
     pedidoContainer: {
         width: '90%',
         backgroundColor: '#ffffff',
-        marginBottom: 100, // separa o container do footer
         padding: 10,
         borderRadius: 5,
         shadowColor: '#000',
@@ -130,39 +145,74 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.1,
         shadowRadius: 5,
         elevation: 5,
+        alignSelf: 'center',
     },
+
     pedidoItem: {
-        marginBottom: 15,
-    },
-    produtoItem: {
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
-        marginBottom: 10,
+        marginBottom: 15,
     },
+
     avatar: {
         width: 60,
         height: 60,
         marginRight: 10,
         borderWidth: 1,
     },
+
     infoContainer: {
         flex: 1,
     },
+
     nome: {
         color: '#74B0FF',
         fontWeight: 'bold',
         fontSize: 14,
     },
+
     preco: {
         color: '#898989',
         fontSize: 12,
     },
+
+    total: {
+        color: '#898989',
+        fontSize: 12,
+    },
+
     quantidade: {
         fontSize: 12,
         color: '#898989',
-        marginRight: 15,
+        marginRight: 25,
+    },
+
+    additionalInfo: {
+        marginTop: 20,
+    },
+
+    greenText: {
+        color: '#26CE55',
+    },
+
+    totalCompra: {
+        color: '#898989',
+        marginTop: 20,
+        fontSize: 16,
+
+    },
+
+    footer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: 60, 
     },
 });
+
+
+
 
 export default HistoricoPedidos;
